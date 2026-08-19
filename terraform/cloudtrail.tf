0f0411c8acabcc0ec258332b4c7b19d36e281733
Hilary Mbamoh Pemamboh <mbamohhilary124@gmail.com>
2026-08-18 19:09:35 -0500

    feat(cloudtrail): enable multi-region audit logging

---
+++ terraform/cloudtrail.tf
@@ -0,0 +1,97 @@
+data "aws_partition" "current" {}
+
+locals {
+  cloudtrail_name = "security-audit-trail"
+  cloudtrail_arn  = "arn:${data.aws_partition.current.partition}:cloudtrail:${var.aws_region}:${data.aws_caller_identity.current.account_id}:trail/${local.cloudtrail_name}"
+}
+
+data "aws_iam_policy_document" "cloudtrail_bucket" {
+  statement {
+    sid     = "AWSCloudTrailAclCheck"
+    effect  = "Allow"
+    actions = ["s3:GetBucketAcl"]
+
+    resources = [aws_s3_bucket.security_logs.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [local.cloudtrail_arn]
+    }
+  }
+
+  statement {
+    sid     = "AWSCloudTrailWrite"
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "${aws_s3_bucket.security_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [local.cloudtrail_arn]
+    }
+  }
+
+  statement {
+    sid     = "DenyInsecureTransport"
+    effect  = "Deny"
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.security_logs.arn,
+      "${aws_s3_bucket.security_logs.arn}/*"
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.security_logs.id
+  policy = data.aws_iam_policy_document.cloudtrail_bucket.json
+}
+
+resource "aws_cloudtrail" "security_audit" {
+  name                          = local.cloudtrail_name
+  s3_bucket_name                = aws_s3_bucket.security_logs.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  enable_logging                = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+
+  depends_on = [aws_s3_bucket_policy.cloudtrail]
+}
